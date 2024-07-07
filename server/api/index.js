57725e902c3bd7ea46a2c862e19f8f4323d9da5e
Larisa Ioana <larisaoltean27@outlook.com>
2024-07-07 13:03:19 +0300

    helmet added

--- server/api/index.js
+++ server/api/index.js
@@ -7,6 +7,7 @@ import videoRoutes from "../routes/videos.js";
 import commentRoutes from "../routes/comments.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import helmet from "helmet";
 
 dotenv.config();
 const app = express();
@@ -15,6 +16,16 @@ app.use(cors({
 	origin: 'http://localhost:5173',
 	credentials: true
 }));
+app.use(helmet({
+	contentSecurityPolicy: {
+		directives: {
+			defaultSrc: ["'self'"],
+			scriptSrc: ["'self'"],
+			objectSrc: ["'none'"]
+		}
+	}
+}));
+
 
 
 const connect = async () => {
