47d6e904383d60a408049c73924031395fdf375f
Larisa Ioana <larisaoltean27@outlook.com>
2024-07-11 11:02:33 +0300

    changes

--- server/index.js
+++ server/index.js
@@ -17,10 +17,7 @@ app.use(cors({
 }));
 const connect = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
+		await mongoose.connect(process.env.MONGO);
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.error(error);
