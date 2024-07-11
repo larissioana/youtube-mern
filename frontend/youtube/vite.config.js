a611f7b0ae020a77833820742bebcda8e6da4296
Larisa Ioana <larisaoltean27@outlook.com>
2024-07-11 16:45:56 +0300

    removing purgecss

--- frontend/youtube/vite.config.js
+++ frontend/youtube/vite.config.js
@@ -5,10 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['react-redux', 'react-infinite-scroll-component', 'redux-persist/integration/react', '@mui/icons-material/VideoLibrary', "redux-persist/lib/storage", 'react-router-dom', 'redux-persist', 'react-modal', '@reduxjs/toolkit'],
-    },
-    minify: true,
-    cssCodeSplit: true,
+    outDir: 'dist',
+
   },
 });
