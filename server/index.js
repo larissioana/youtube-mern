ec98edb5a73a3dc895ecb83d15b325ebdee34782
Larisa Ioana <larisaoltean27@outlook.com>
2024-07-12 10:29:55 +0300

    api changed

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-	origin: 'https://youtube-mern-git-main-larissioanas-projects.vercel.app/',
+	origin: 'https://youtube-mern-git-main-larissioanas-projects.vercel.app',
 	credentials: true
 }));
 const connect = async () => {
